d89dca4dc3cd200eb2f5b01c68786217ebe2c4b4
Rafael Antunes <rmrantunes.dev@gmail.com>
2025-09-10 10:16:31 -0400

    feat: initial add channels basics

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,19 @@
 package org.example
 
 import kotlinx.coroutines.runBlocking
+import org.example.corountines.ChannelsBasics
 import org.example.corountines.CoroutinesBasics
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     runBlocking { // this: CoroutineScope, bridges coroutine world with main thread world, blocking the thread
-        CoroutinesBasics.helloWorld2()
-        CoroutinesBasics.helloWorld2()
-        println("Done")
-        CoroutinesBasics.explicitJob()
-        CoroutinesBasics.lightWeight()
+//        CoroutinesBasics.helloWorld2()
+//        CoroutinesBasics.helloWorld2()
+//        println("Done")
+//        CoroutinesBasics.explicitJob()
+//        CoroutinesBasics.lightWeight()
+
+        ChannelsBasics.simpleChannels()
     }
 }
---
+++ src/main/kotlin/corountines/ChannelsBasics.kt
@@ -0,0 +1,42 @@
+package org.example.corountines
+
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+
+object ChannelsBasics {
+    fun channelTypes() {
+//        val rendezvousChannel = Channel<String>()
+//        val bufferedChannel = Channel<String>(10)
+//        val conflatedChannel = Channel<String>(CONFLATED)
+//        val unlimitedChannel = Channel<String>(UNLIMITED)
+    }
+
+    suspend fun simpleChannels() = coroutineScope {
+        val channel = Channel<String>()
+
+        launch {
+            channel.send("A1")
+            channel.send("A2")
+            channel.send("A3")
+            log("A done")
+        }
+
+        launch {
+            channel.send("B1")
+            channel.send("B2")
+            log("B done")
+        }
+
+        launch {
+            repeat(5) {
+                val x = channel.receive()
+                log(x)
+            }
+        }
+    }
+
+    private fun log(message: Any?) {
+        println("[${Thread.currentThread().name}] $message")
+    }
+}

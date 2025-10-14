9742d80311533205d91024e90ce861897af9911f
Rafael Antunes <rmrantunes.dev@gmail.com>
2025-10-14 17:09:15 -0400

    feat: add more stuff to coroutine asynchronous flow

--- src/test/kotlin/corountines/AsynchronousFlow.kt
+++ src/test/kotlin/corountines/AsynchronousFlow.kt
@@ -1,11 +1,10 @@
 package org.example.corountines
 
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.forEach
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withTimeoutOrNull
 import kotlin.test.Test
 
 class AsynchronousFlow {
@@ -62,5 +61,93 @@ class AsynchronousFlow {
         flow.collect { value -> println(value) }
         println("Calling collect again...")
         flow.collect { value -> println(value) }
+    }
+
+    @Test
+    fun flowCancellationBasics() = runTest {
+        fun simple(): Flow<Int> = flow {
+            println("Flow started")
+            for (i in 1..3) {
+                delay(100L)
+                println("Emitting $i")
+            }
+        }
+
+        withTimeoutOrNull(250L) {
+            simple().collect { value -> println(value) }
+        }
+
+        println("Done")
+    }
+
+    @Test
+    fun flowBuilders() = runTest {
+        flowOf(1, 2, 3).collect { value -> println("flowOf: $value") }
+        (1..3).asFlow().collect { value -> println("asFlow: $value") }
+    }
+
+    @Test
+    fun intermediateFlowOperations() = runTest {
+        suspend fun performRequest(id: Int): String {
+            delay(1000L)
+            return "request #$id"
+        }
+
+        (1..3).asFlow()
+            .map { value -> performRequest(value) }
+            .collect { value -> println(value) }
+    }
+
+    @Test
+    fun transform() = runTest {
+        suspend fun performRequest(id: Int): String {
+            delay(1000L)
+            return "request #$id"
+        }
+
+        (1..3).asFlow()
+            .transform { value ->
+                emit("Making request #$value")
+                emit(performRequest(value))
+            }
+            .collect { value -> println(value) }
+    }
+
+    @Test
+    fun sizeLimitingOperators() = runTest {
+        fun numbers() = flow<Int> {
+            try {
+                emit(1)
+                emit(2)
+                println("This line will not execute")
+                emit(3)
+            } finally {
+                println("Finally in numbers")
+            }
+        }
+
+        numbers().take(2).collect { value -> println(value) }
+    }
+
+    @Test
+    /**
+     * @see - https://kotlinlang.org/docs/flow.html#terminal-flow-operators
+     * */
+    fun terminalFlowOperators() = runTest {
+        val sum = (1..5).asFlow().map { it * it }.reduce { a, b -> a + b }// sum them (terminal operator)
+        println(sum)
+    }
+
+    @Test
+    fun flowsAreSequential() = runTest {
+        (1..5).asFlow().filter {
+            println("filter: $it")
+            it % 2 == 0
+        }.map {
+            println("map: $it")
+            "string $it"
+        }.collect {
+            println("collect: $it")
+        }
     }
 }

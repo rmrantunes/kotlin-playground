fa4f8a83e88f91417c3e3d6f94edab0dfb1804df
Rafael Antunes <rmrantunes.dev@gmail.com>
2025-10-14 14:25:40 -0400

    refactor: add functions as test methods
    idk

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,47 +1,6 @@
 package org.example
 
-import kotlinx.coroutines.runBlocking
-import org.example.corountines.CancellationAndTimeout
-import org.example.corountines.ChannelsBasics
-import org.example.corountines.ComposingSuspendingFns
-import org.example.corountines.ContextAndDispatchers
-import org.example.corountines.CoroutinesBasics
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    runBlocking { // this: CoroutineScope, bridges coroutine world with main thread world, blocking the thread
-//        CoroutinesBasics.helloWorld2()
-//        CoroutinesBasics.helloWorld2()
-//        println("Done")
-//        CoroutinesBasics.explicitJob()
-//        CoroutinesBasics.lightWeight()
-
-//        CancellationAndTimeout.cancellingExecution()
-//        CancellationAndTimeout.cancellationIsCooperative1()
-//        CancellationAndTimeout.cancellationIsCooperative2()
-//        CancellationAndTimeout.makingComputationCodeCancellable()
-//        CancellationAndTimeout.closingResourcesWithFinally()
-//        CancellationAndTimeout.runNonCancellableBlock()
-//        CancellationAndTimeout.timeoutOrNull()
-//        CancellationAndTimeout.asynchronousTimeoutAndResources()
-//        CancellationAndTimeout.asynchronousTimeoutAndResourcesWorkaround()
-
-//        ComposingSuspendingFns.sequentialByDefault()
-//        ComposingSuspendingFns.concurrentUsingAsync()
-//        ComposingSuspendingFns.lazyStartedAsync()
-//        ComposingSuspendingFns.structuredConcurrencyWithAsync()
-//        ComposingSuspendingFns.cancellationPropagation()
-//        ComposingSuspendingFns.asyncStyleFunctions()
-
-//        ContextAndDispatchers.simple()
-        //ContextAndDispatchers.unconfinedVsConfined()
-        // ContextAndDispatchers.jumpingBetweenThreads()
-        //ContextAndDispatchers.jobInTheContext()
-        // ContextAndDispatchers.childrenOfCoroutine()
-        // ContextAndDispatchers.combiningContextElements()
-        ContextAndDispatchers.threadLocalData()
-
-    }
-
 }

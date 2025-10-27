a4d533a10f28c4356a7a75914dd94ddf2b304283
Rafael Antunes <rmrantunes.dev@gmail.com>
2025-10-27 17:47:04 -0400

    feat: idk some tests

--- src/test/kotlin/corountines/Channels.kt
+++ src/test/kotlin/corountines/Channels.kt
@@ -1,48 +1,54 @@
 package org.example.corountines
 
+import kotlin.test.Test
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.withTimeoutOrNull
 import org.example.coroutines.OrderProcessingChallenge
 import org.example.coroutines.StockMarketRouterChallenge
-import kotlin.test.Test
 
 class Channels {
     @Test fun orderProcessingChallenge() = runTest { OrderProcessingChallenge().execute() }
 
-    @Test fun stockMarketRouterChallenge() = runBlocking { StockMarketRouterChallenge().execute() }
+    @Test fun stockMarketRouterChallenge() = runTest { StockMarketRouterChallenge().execute() }
 
     @Test
     fun mutableSharedFlow() = runTest {
         withTimeoutOrNull(5000) {
-            val broadcast =
-                MutableSharedFlow<Int>(1)
+            val broadcast = MutableSharedFlow<Int>(1)
 
             launch {
-                broadcast.collect {
-                    delay(100)
-                    println("#1 $it")
-                }
+                broadcast
+                    .onEach { println("#1 entered $it") }
+                    .collect {
+                        delay(300)
+                        println("#1 $it")
+                    }
             }
             launch {
                 // while (true) {
                 repeat(100) {
-                    delay(50)
+                    //delay(50)
                     broadcast.emit(it + 1)
                 }
                 // }
             }
 
-            launch { delay(300)
+            launch {
+                delay(300)
 
-                broadcast.collect { println("#2 $it") } }
+                broadcast.collect { println("#2 $it") }
+            }
 
-            launch { delay(700)
+            launch {
+                delay(700)
 
-                broadcast.collect { println("#3 $it") } }
+                broadcast.collect { println("#3 $it") }
+            }
         }
     }
 }
